bf214cd27ea179ddb0b3642b31ed511d371445e3
dan.reia@gmail.com <q>
2022-07-08 15:27:30 +0200

    Base CLI framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: FirstSubcommand,
+}
+
+#[derive(Subcommand)]
+enum FirstSubcommand {
+    /// Initialize project configuration
+    Init {},
+    /// Get Oauth Token
+    Login {},
+    /// Perform operations on accounts
+    Accounts {
+        #[clap(subcommand)]
+        name: AccountOptions,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+enum AccountOptions {
+    /// Check account balance
+    Balance {},
+    /// List accounts
+    List {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        FirstSubcommand::Init {} => {
+            todo!("Initialize .investecrc");
+        }
+        FirstSubcommand::Login {} => {
+            todo!("Log in");
+        }
+        FirstSubcommand::Accounts { name } => match &name {
+            AccountOptions::Balance {} => {
+                todo!("Display account balances here");
+            }
+            AccountOptions::List {} => {
+                todo!("List accounts here");
+            }
+        },
+    }
 }
